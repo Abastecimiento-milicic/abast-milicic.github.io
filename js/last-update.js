2d8854acde32e8735f5572e7564fa55443c66b98
dukke89 <david.benitez@milicic.com.ar>
2026-09-10 13:02:40 -0300

    ACT SERVICIOS

--- js/last-update.js
+++ js/last-update.js
@@ -8,7 +8,7 @@ if (!cb) {
   cb = new Date().getTime();
   sessionStorage.setItem('mi_cache_buster', cb);
 }
-window.CACHE_BUSTER = cb + "_v14_periodo_corto";
+window.CACHE_BUSTER = cb + "_v15_periodo_corto";
 
 window.forceRefreshData = function() {
   sessionStorage.removeItem('mi_cache_buster');

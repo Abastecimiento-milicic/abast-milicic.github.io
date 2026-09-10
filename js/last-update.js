4bfe45efb46d4a87a449502ac5d592ed2e37b420
dukke89 <david.benitez@milicic.com.ar>
2026-09-10 12:29:42 -0300

    ACT 2

--- js/last-update.js
+++ js/last-update.js
@@ -8,7 +8,7 @@ if (!cb) {
   cb = new Date().getTime();
   sessionStorage.setItem('mi_cache_buster', cb);
 }
-window.CACHE_BUSTER = cb + "_v13_periodo_corto";
+window.CACHE_BUSTER = cb + "_v14_periodo_corto";
 
 window.forceRefreshData = function() {
   sessionStorage.removeItem('mi_cache_buster');

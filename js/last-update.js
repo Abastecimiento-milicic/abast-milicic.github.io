e37a812d514590135b40761db06e7548ef5240d6
Abastecimiento-milicic <david.benitez@milicic.com.ar>
2026-09-24 16:35:55 -0300

    ACT CUMPLIMIENTO

--- js/last-update.js
+++ js/last-update.js
@@ -8,7 +8,7 @@ if (!cb) {
   cb = new Date().getTime();
   sessionStorage.setItem('mi_cache_buster', cb);
 }
-window.CACHE_BUSTER = cb + "_v26_periodo_corto";
+window.CACHE_BUSTER = cb + "_v27_periodo_corto";
 
 window.forceRefreshData = function() {
   sessionStorage.removeItem('mi_cache_buster');
